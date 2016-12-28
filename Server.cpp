8f59c55b451f29a405285a36e0c3854133a2467c
mingz <305603665@qq.com>
2016-12-28 20:26:19 +0800

    init

---
+++ Server.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zhaojm on 28/12/2016.
+//
+
+#include "Server.h"
---
+++ Server.h
@@ -0,0 +1,14 @@
+//
+// Created by zhaojm on 28/12/2016.
+//
+
+#ifndef SERVER_BASE_SERVER_H
+#define SERVER_BASE_SERVER_H
+
+
+class Server {
+
+};
+
+
+#endif //SERVER_BASE_SERVER_H
